32f8c24a67ca29eef3f3598d63c8e0dd55cc961c
Triono <triono2125@gmail.com>
2022-01-24 16:42:56 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const logger = require('morgan');
+const router = require('./App/products/routes');
+const productRouter = require('./App/products/routes');
+const productRouterV1 = require('./App/product-v1/routes-v2');
+const productRouterV2 = require('./App/product-v2/routes');
+
+// Module Router
+app.use(router);
+
+app.use(logger('dev'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+// Handle File Static
+app.use('/public', express.static(path.join(__dirname, 'uploads', )));
+
+// Product Router
+app.use('/api/v1', productRouter);
+app.use('/api/v2', productRouterV1);
+app.use('/api/v2', productRouterV2);
+
+// Handle Error 404
+app.use((req, res, next) => {
+    res.status(404);
+    res.send({
+        status: 'Failed',
+        message: 'Resource ' + req.originalUrl + ' Not Found'
+    });
+});
+
+app.listen(3001, () => console.log('Server: http://localhost:3001'));
